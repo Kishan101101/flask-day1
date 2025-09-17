2bbb3291150bf9dacd0e4c2008f72c390b722c77
Kishan Yadav <kishan.yadav@opalina.in>
2025-09-17 13:20:43 +0530

    First Flask app

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,redirect,url_for,session,Response,render_template
+app = Flask(__name__)
+app.secret_key='secret_key'
+@app.route('/',methods=['GET','POST'])
+def Login():
+    if request.method == 'POST':
+       username = request.form.get('username')
+       password = request.form.get('password')
+       if username == "admin" and password == "12345":
+                session['user'] = username
+                session['logged_in'] = True
+                return 'Logged in successfully!'
+       else:
+              return "Invalid credentials. Please try again."
+       
+    return render_template('Login.html')
+@app.route('/userdetails',methods=['GET'])
+def userdetails():
+         if 'user' in session:
+            #    print(session['user'])
+               return render_template('userdetails.html',username=session['user'])
+         else:
+                return redirect(url_for('Login'))
+         
+@app.route('/logout',methods=['GET'])
+def Logout():
+      session.pop('user',None)
+      return Response('Logged out successfully!')
+
+if __name__ == '__main__':
+    app.run(debug=True)
